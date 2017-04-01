095fe69be0afa2a45f3b42e8041a9b12723eeee4
leo <luqizheng@126.com>
2017-04-01 14:02:17 +0800

    修正发布bat

--- pack.release.bat
+++ pack.release.bat
@@ -1,8 +1,21 @@
-dotnet pack src/Ornament.Domain -c:release -o:./
-dotnet pack src/Ornament -c:release -o:./
-dotnet pack src/Uow/Ornament.Uow.DbConnection -c:release -o:./
-dotnet pack src/Ornament.Uow.Web -c:release -o:./
-dotnet pack src/Uow/Ornament.Uow.Ef -c:release -o:./
-dotnet pack src/Uow/Ornament.Uow.NHibernate -c:release -o:./
-rem del *.nupkg
-copy *.nupkg c:\inetpub\wwwroot\packages /y
+call:pack src/Ornament.Domain 
+call:pack src/Ornament 
+call:pack src/Uow/Ornament.Uow.DbConnection 
+call:pack src/Ornament.Uow.Web
+call:pack src/Uow/Ornament.Uow.Ef 
+call:pack src/Uow/Ornament.Uow.NHibernate 
+
+
+:pack
+dotnet restore %1
+dotnet pack -c:release -o:%cd%  %1
+
+GOTO:EOF
+
+:publish
+set /p nuget=«Î ‰»Îurl:
+set /p webkey=«Î ‰»ÎapiKey:
+for %%i in (*.nupkg) do (
+ dotnet nuget push -s %nuget% %%i -k %webkey%
+)
+goto:eof
